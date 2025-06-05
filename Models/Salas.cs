2e5f3b369eeb1f782666794455bae82bc38b9628
IVAN MOSCOVICH LEVI <48802641@ort>
2025-06-05 14:20:08 -0300

    Puse imagenes

---
+++ Models/Salas.cs
@@ -0,0 +1,16 @@
+using Newtonsoft.Json;
+
+public class Sala
+{
+    public int Numero { get; set; }
+    public string Titulo { get; set; }
+    public string Narrativa { get; set; }
+    public string Pista { get; set; }
+    public string ClaveCorrecta { get; set; }
+}
+public  void iniciarGrupo()
+    {
+        
+
+
+    }
--- obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c3a513fad2d23e12f6a37c0ca58b183b1d049e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
